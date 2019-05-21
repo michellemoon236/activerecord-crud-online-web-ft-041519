ec580af7dbcb77e1fa08a8688d8211efbc31ba09
Michelle Moon <michellemoon236@gmail.com>
2019-05-21 23:12:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    Movie.new(title, release_date)
+    Movie.create(title, release_date)
   end
 end
 
